120c041519c316e341cb1553dadaa23c975644a1
tamilarasanfple <tamilarasan@fple.in>
2024-04-12 05:17:05 +0000

    9th

--- gitpull.sh
+++ gitpull.sh
@@ -9,8 +9,13 @@ repositories=(
 echo "Pulling changes from Git repository"
 git pull origin main
 
+# Show changes pulled from the repository
+echo "Changes pulled from the Git repository:"
+git status
+
 # Loop through each repository
 for repo_path in "${repositories[@]}"; do
+
     echo "Bringing down containers for repository at: ${repo_path}"
     sudo docker-compose -f "$repo_path/docker-compose.yml" down
 
