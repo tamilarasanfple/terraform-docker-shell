deff1e41fe8ea5eac42bb7d1574b3208fd0236f9
tamilarasanfple <tamilarasan@fple.in>
2024-04-12 04:44:57 +0000

    6th

--- gitpull.sh
+++ gitpull.sh
@@ -8,12 +8,12 @@ repositories=(
 # Loop through each repository
 for repo_path in "${repositories[@]}"; do
     echo "Bringing down containers for repository at: ${repo_path}"
-    docker-compose -f "$repo_path/docker-compose.yml" down
+    sudo docker-compose -f "$repo_path/docker-compose.yml" down
 
     echo "Bringing up containers for repository at: ${repo_path}"
-    docker-compose -f "$repo_path/docker-compose.yml" up -d
+    sudo docker-compose -f "$repo_path/docker-compose.yml" up -d
 
     echo "Checking running containers for repository at: ${repo_path}"
-    docker ps -a
+    sudo docker ps -a
 done
 
