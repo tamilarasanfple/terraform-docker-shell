6584480af3f47eb2e298ff7a338b02c27f6ec13e
tamilarasanfple <tamilarasan@fple.in>
2024-04-15 03:56:15 +0000

    10th

--- gitpull.sh
+++ gitpull.sh
@@ -22,8 +22,3 @@ for repo_path in "${repositories[@]}"; do
     echo "Bringing up containers for repository at: ${repo_path}"
     sudo docker-compose -f "$repo_path/docker-compose.yml" up -d
 done
-
-# Display status of all Docker containers
-echo "Current Docker container status:"
-sudo docker ps -a
-
